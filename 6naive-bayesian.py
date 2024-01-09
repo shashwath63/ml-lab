184441542f527a5ccca54d257893811f3cdab103
shashwath63 <shashwathbn10@gmail.com>
2024-01-09 18:30:48 +0530

    naive bayesian

--- 6naive-bayesian.py
+++ 6naive-bayesian.py
@@ -1,16 +1,86 @@
-from sklearn.naive_bayes import GaussianNB 
-from sklearn.datasets import load_iris 
-from sklearn import metrics 
-from sklearn.model_selection import train_test_split
+import csv
+import random
+import math
 
-dataset = load_iris()
-X = dataset. data
-y = dataset. target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-gnb = GaussianNB()
-classifier = gnb.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-print( 'Accuracy metrics:', metrics.classification_report(y_test, y_pred))
-print( 'Accuracy of the classifier is:', metrics.accuracy_score(y_test, y_pred))
-print("Confusion matrix:")
-print(metrics.confusion_matrix(y_test, y_pred))
+def loadcsv(filename):
+    dataset = list(csv.reader(open(filename,"r")))
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    trainSet,testSet = dataset[:trainSize],dataset[trainSize:]
+    return [trainSet, testSet]
+
+def mean(numbers):
+    return sum(numbers)/(len(numbers))
+
+def stdev(numbers):
+    avg = mean(numbers)
+    v = 0
+    for x in numbers:
+        v += (x-avg)**2
+    return math.sqrt(v/(len(numbers)-1))
+
+def summarizeByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    summaries = {}
+    for classValue, instances in separated.items():
+        summaries[classValue] = [(mean(attribute), stdev(attribute)) for attribute in zip(*instances)][:-1]
+    return summaries
+
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp((-(x-mean)**2)/(2*(stdev**2)))
+    return (1 / math.sqrt(2*math.pi*(stdev**2))) * exponent
+
+def predict(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet[i][-1] == predictions[i]:
+            correct += 1
+    return (correct/(len(testSet))) * 100.0
+
+filename = 'diabetes2.csv'
+splitRatio = 0.9
+dataset = loadcsv(filename)
+actual = []
+trainingSet, testSet = splitDataset(dataset, splitRatio)
+for i in range(len(testSet)):
+	vector = testSet[i]
+	actual.append(vector[-1])
+print('Split {0} rows into train={1} and test={2} rows'.format(len(dataset), len(trainingSet), len(testSet)))
+summaries = summarizeByClass(trainingSet) #will have (mean,sd) for all attributes.(for class 1 & 0 separately)
+predictions = getPredictions(summaries, testSet)
+print('\nActual values:\n',actual)
+print("\nPredictions:\n",predictions)
+accuracy = getAccuracy(testSet, predictions)
+print("Accuracy",accuracy)
